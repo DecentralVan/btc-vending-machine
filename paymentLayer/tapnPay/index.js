84d0c59eb1187365e3dc902d64277ba4a53c4840
Taylor Rhodes <taylorsingletonfookes@live.com>
2017-06-09 16:17:56 -0700

    bounty tap

--- paymentLayer/tapnPay/index.js
+++ paymentLayer/tapnPay/index.js
@@ -19,43 +19,147 @@ function keyparse(code) {
   var key = code.substr(4);
   if (key == "ENTER") {
     console.log(fob);
-    checkWithBrain(fob)
-    fob = ""
+    bountyClaimProcess(fob, isHandled => {
+      if (!isHandled) {
+        console.log("Trying to charge for beer")
+        checkWithBrain(fob)
+      }
+      fob = ""
+    })
   } else {
     fob = fob + key;
   }
 }
 
-reader.on("EV_KEY",function(data){
+reader.on("EV_KEY", function(data) {
   if (data.value == 1)
     keyparse(data.code)
 });
+
+function calculatePayout(monthValue, lastClaimed, now){
+    let msSince = now - lastClaimed
+    let today = new Date()
+    let daysThisMonth = new Date(today.getYear(), today.getMonth(), 0).getDate()
+    let msThisMonth = daysThisMonth * 24 * 60 * 60 * 1000
+    return (msSince / msThisMonth) * monthValue
+}
+
+// return if handled by bounty stoping
+// testing?
+// bountyClaimProcess("0005826993", isHandled => {
+//     console.log({isHandled})
+//     bountyClaimProcess("0006047467", isHandled => {
+//         console.log({isHandled})
+//     })
+// })
+
+let claimRequest  = {
+  action: {
+    type: "bounty-claimed",
+  }
+}
+
+let payoutRequest = {
+  action: {
+    type: "member-paid",
+    "cash?": false,
+  }
+}
+
+var activeBounty = false
+function bountyClaimProcess(scannedFob, isHandledCallback) {
+  if (activeBounty) {
+      // Have an active bounty so next tap is to claim bounty:
+      request
+          .get(brainLocation + 'members/' + scannedFob)
+          .end((err, res) => {
+            if (err || res.body.error) {
+              console.log('Invalid Fob')
+              return null
+            }
+
+            payoutRequest.action["address"] = res.body.address
+            claimRequest.action["address"] = res.body.address
+            claimRequest.action["notes"] = Date.now().toString()
+
+            console.log({payoutRequest, claimRequest})
+
+            request
+                .post(brainLocation + 'members')
+                .send(payoutRequest)
+                .end((err, res) => {
+                    if (err || res.body.error) {
+                      console.log('Unable to create')
+                      return null
+                    }
+                    console.log(res.body)
+                })
+
+            request
+                .post(brainLocation + 'bounties')
+                .send(claimRequest)
+                .end((err, res) => {
+                    if (err || res.body.error) {
+                      console.log('Unable to create')
+                      return null
+                    }
+                    console.log(res.body)
+                })
+
+
+
+          })
+  }
+
+  request
+    .get(brainLocation + 'bounties/' + scannedFob)
+    .end((err, res) => {
+      if (err || res.body.error) {
+        console.log('Invalid Fob')
+        return isHandledCallback(false)
+      }
+      activeBounty = res.body
+      console.log("res to bounties/:fob", res.body)
+      let now = Date.now()
+      let monthValue = res.body.value
+      let lastClaimed = res.body.notes
+      let amount = calculatePayout(monthValue, lastClaimed, now)
+      // Build in the info we need from the bounty, next tap will send these requests
+      claimRequest.action["bounty-id"] = res.body["bounty-id"]
+      payoutRequest.action["notes"] = res.body["bounty-id"]
+      payoutRequest.action["amount"] = amount.toString()
+      // This was a bounty tag so we do not need to check for beer
+      isHandledCallback(true)
+    })
+}
 
 function checkWithBrain(scannedFob) {
   request
     .get(brainLocation + 'members/' + scannedFob)
     .end((err, res) => {
-      if (err || res.body.error){
-          console.log('Invalid Fob')
-          return null
+      if (err || res.body.error) {
+        console.log('Invalid Fob')
+        return null
       }
-      console.log("address?" , res.body.address)
+      console.log("address?", res.body.address)
       let chargeRequest = {
-	action: {
-        type: "member-charged",
-        address: res.body.address,
-        amount: "3",
-        notes: "BitPepsi"
+        action: {
+          type: "member-charged",
+          address: res.body.address,
+          amount: "3",
+          notes: "BitPepsi"
         }
       }
-      console.log({chargeRequest})
+      console.log({
+        chargeRequest
+      })
       request
         .post(brainLocation + 'members')
         .send(chargeRequest)
         .end((err, res) => {
-          if (err || res.body.error){
-              console.log('Unable to create')
-              return null
+          if (err || res.body.error) {
+            console.log('Unable to create')
+            return null
           }
           emit(1)
         })
